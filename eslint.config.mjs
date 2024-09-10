4033f6277ca14f24e84eb21746685a3d47c41858
Akshay <rathodak315@gmail.com>
2024-09-10 13:26:18 +0530

    added  prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,38 @@
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/dist", "**/node_modules", "**/.env*.local"],
-}, ...compat.extends("plugin:@typescript-eslint/recommended"), {
+export default [
+  {
+    ignores: ['**/dist', '**/node_modules', '**/.env*.local'],
+  },
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
 
     rules: {
-        "no-console": "off",
-        "import/prefer-default-export": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
+      'no-console': 'off',
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
-}];
+  },
+];
