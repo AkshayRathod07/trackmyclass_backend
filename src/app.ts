63083a6809f92fbfdc8c783284bac40660a9adc1
rohya-28 <rohitarsude28@gmail.com>
2024-10-08 15:06:03 +0530

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import globalErrorHandler from './middleware/globalErrorHandler';
 import userRouter from './routes/user';
 import collegeRouter from './routes/college';
+import cors from 'cors';
+import { config } from './db/config';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: config.frontEndDomain,
+  })
+);
+
 app.use(express.json());
 
 // Routes
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'welcome to routes' });
 });
 
